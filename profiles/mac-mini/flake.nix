033c3de428ab08636476771297007da009ec5477
Zettl Robert <zettl.robert@gmail.com>
2026-05-20 13:56:33 +0200

    refactor: migrate mac-mini profile to follow core/nixpkgs

---
+++ profiles/mac-mini/flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Flake for `mac-mini` system packages";
+
+  inputs = {
+    core.url = "path:../../packages/core-packages";
+    nixpkgs.follows = "core/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, core }:
+    let
+      # Systems supported
+      allSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+
+      # Helper to provide system-specific attributes
+      forAllSystems = f:
+        nixpkgs.lib.genAttrs allSystems (system:
+          f {
+            pkgs = import nixpkgs {
+              inherit system;
+              config = { allowUnfree = true; };
+            };
+          });
+    in
+    {
+      packages = forAllSystems ({ pkgs }: {
+        default = pkgs.buildEnv {
+          name = "mac-mini";
+          paths = with pkgs; [
+            core.packages.${pkgs.system}.default
+
+            # macOS-specific extras
+            # https://exiftool.org/
+            perl538Packages.ImageExifTool
+          ];
+        };
+      });
+    };
+}
